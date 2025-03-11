211c132a53f0c997bc3828ec48f2bcb45bb3056d
Gabriel Steffens <gabrielsteffens2003@gmail.com>
2025-03-10 23:14:06 -0300

    projeto 1.0 concluído (falta alinhar a responsividade)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 //Seleção de elementos
 const scrollUp = document.getElementById("scroll-icon"); //seta de scroll up no fim da página
+
 const homePageButton = document.getElementById("home-icon"); // botão que direciona para a home page
-const projectsButton = document.getElementById("projects-icon"); //Dom para o botão que direciona para a aba de projeto
 const homePage = document.querySelector(".sobre-mim"); //Dom para a home page
+
+const projectsButton = document.getElementById("projects-icon"); //Dom para o botão que direciona para a aba de projeto
 const projectsPage = document.querySelector(".projetos"); //Dom para a aba de projetos
+
+const contactButton = document.getElementById("contact-icon"); //Dom para o botão que direciona para a aba de contato
+const contactPage = document.querySelector(".contact"); //Dom para a aba de contato
 
 
 //Atribuição de eventos
@@ -18,10 +23,18 @@ scrollUp.addEventListener("click", function(){
 
 projectsButton.addEventListener("click", function(){
     homePage.classList.add("none");
-    projectspPage.classList.remove("none");
+    contactPage.classList.add("none");
+    projectsPage.classList.remove("none");
 });
 
 homePageButton.addEventListener("click", function(){
     projectsPage.classList.add("none");
+    contactPage.classList.add("none");
     homePage.classList.remove("none");
 });
+
+contactButton.addEventListener("click", function(){
+    homePage.classList.add("none");
+    projectsPage.classList.add("none");
+    contactPage.classList.remove("none");
+}); 
